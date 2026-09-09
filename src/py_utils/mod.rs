de618ee40ae46527f98abc9c332f7df8790b160a
bglid <benjamin.h.glidden@gmail.com>
2026-09-09 07:04:01 -0400

    Making macros exist in separate module

---
+++ src/py_utils/mod.rs
@@ -0,0 +1,7 @@
+#![allow(
+    clippy::pub_with_shorthand,
+    reason = "Project prefers shorthand visibility syntax"
+)]
+
+mod macros;
+pub(crate) use macros::*;
--- src/python.rs
+++ src/python.rs
@@ -12,6 +12,7 @@ use numpy::{PyArray1, PyReadonlyArray1};
 use crate::Error;
 use crate::algorithms::{Algorithm, LeastMeanSquares, NormalizedLeastMeanSquares};
 use crate::filters::FilterBase;
+use crate::py_utils::generate_filter_bindings;
 use crate::types::{InputSignal, NoiseReference};
 
 impl Error {
@@ -83,54 +84,6 @@ where
 mod adaptif {
     #[pymodule_export]
     use super::{LMSFilter, NLMSFilter};
-}
-
-/// Declarative macro for generating filter binding setup.
-macro_rules! generate_filter_bindings {
-    ($name: ident) => {
-        #[pymethods]
-        impl $name {
-            #[getter]
-            fn window_size(&self) -> usize {
-                self.0.window_size()
-            }
-
-            // TODO: weights() (+ check before/after in tests)
-
-            fn adapt<'py>(
-                &mut self,
-                py: Python<'py>,
-                input_signal: PyReadonlyArray1<f64>,
-                noise_ref: PyReadonlyArray1<f64>,
-            ) -> PyResult<Bound<'py, PyArray1<f64>>> {
-                adapt_filter_impl(
-                    &mut self.0,
-                    py,
-                    input_signal,
-                    noise_ref,
-                    FilterOperation::Adapt,
-                )
-            }
-
-            fn filter<'py>(
-                // self has to be mutable so that we can call adapt_filter_impl().
-                // In Python there is no immutability, and we later pass an immutable reference
-                // to the Rust filter() fn with the actual implementation, so this is fine.
-                &mut self,
-                py: Python<'py>,
-                input_signal: PyReadonlyArray1<f64>,
-                noise_ref: PyReadonlyArray1<f64>,
-            ) -> PyResult<Bound<'py, PyArray1<f64>>> {
-                adapt_filter_impl(
-                    &mut self.0,
-                    py,
-                    input_signal,
-                    noise_ref,
-                    FilterOperation::Filter,
-                )
-            }
-        }
-    };
 }
 
 // ------- LMS
